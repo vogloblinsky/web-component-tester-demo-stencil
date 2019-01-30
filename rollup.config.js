8b6c76c25ceac24e318830540258cef944fa9845
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2019-01-30 11:21:20 +0100

    Simple test es5

---
+++ rollup.config.js
@@ -0,0 +1,8 @@
+export default {
+    input: 'test-es5-code/index.js',
+    context: 'window',
+    output: {
+        dir: 'test-es5-code/build',
+        format: 'cjs'
+    }
+}
---
+++ test-es5-code/index.js
@@ -0,0 +1,4 @@
+import {
+    defineCustomElements
+} from '../dist/loader/index';
+defineCustomElements(window);
