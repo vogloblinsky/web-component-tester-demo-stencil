44f003a598c078f418f187bd6dc5adf2da8b6200
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2019-01-30 17:20:15 +0100

    More test

--- test/nova_test.js
+++ test/nova_test.js
@@ -6,8 +6,19 @@ defineCustomElements(window);
 const assert = chai.assert;
 describe('Nova badge', () => {
     let element;
+    let scoreElement;
+    let canShadowDom;
     before(() => {
         element = document.querySelector('nova-badge');
+        canShadowDom = !!(
+            document.createElement('div').attachShadow ||
+            document.createElement('div').createShadowRoot
+        );
+        if (canShadowDom) {
+            scoreElement = element.shadowRoot.querySelector('.score');
+        } else {
+            scoreElement = document.querySelector('nova-badge .score');
+        }
     });
     it('Rendered by Stencil', () => {
         const testPromise = new Promise(function(resolve) {
@@ -18,5 +29,11 @@ describe('Nova badge', () => {
         return testPromise.then(function(result) {
             assert.include(result.split(' '), 'hydrated');
         });
+    });
+    it('should have the nova badge score to 1', () => {
+        assert.equal(scoreElement.textContent, "1");
+    });
+    it('should have the nova badge score correct CSS class', () => {
+        assert.notEqual(scoreElement.getAttribute('class').indexOf('one'), -1);
     });
 });
