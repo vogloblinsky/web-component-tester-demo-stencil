ce109ad18335bd74175b67dc48aad2f8ad81e74d
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2019-01-30 11:56:39 +0100

    Test saucelabs

--- wct.conf.js
+++ wct.conf.js
@@ -13,25 +13,39 @@ module.exports = {
             "browsers": [{
                     "browserName": "chrome",
                     "platform": "Windows 10",
-                    "version": ""
+                    "version": "71"
                 }, {
                     "browserName": "MicrosoftEdge",
                     "platform": "Windows 10",
-                    "version": ""
+                    "version": "18"
                 }, {
                     "browserName": "Safari",
                     "platform": "macOS 10.13",
                     "version": "11"
-                },
+                }, {
+                    "browserName": "Safari",
+                    "platform": "macOS 10.12",
+                    "version": "10.1"
+                }/*,
                 {
                     "browserName": "internet explorer",
                     "platform": "Windows 10",
                     "version": "11"
-                },
+                }*/,
                 {
                     "browserName": "firefox",
                     "platform": "macOS 10.13",
                     "version": ""
+                },
+                {
+                    "browserName": "firefox",
+                    "platform": "macOS 10.13",
+                    "version": "64.0"
+                },
+                {
+                    "browserName": "firefox",
+                    "platform": "macOS 10.13",
+                    "version": "62.0"
                 }
             ]
         }
